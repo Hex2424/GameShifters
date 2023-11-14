84694c52f94b2f046733679fbac0b0f99f82ab82
Eimantas Katinas <katinaseimantas@gmail.com>
2023-11-14 22:16:53 +0200

    Added games and account info

--- main.py
+++ main.py
@@ -3,27 +3,56 @@ from flask import Flask, request, make_response, jsonify, render_template, redir
 from decouple import config
 from steam import Steam
 from pysteamsignin.steamsignin import SteamSignIn
+import requests
+from multiprocessing import Pool
+import pymongo
 
 app = Flask(__name__, template_folder='html', static_folder='css')
 
 KEY = config("STEAM_API_KEY")
 steam = Steam(KEY)
 
+mongodb = pymongo.MongoClient(config("MONGO_URI"))
+database = mongodb.game_shifters
+
 @app.route('/')
 def main():
     steamID = request.cookies.get('steam_id')
+
+    # recommended_games = []
+    # recommended_games_json = requests.get('https://steamspy.com/api.php?request=top100in2weeks').json()
+    # game_ids = list(recommended_games_json.keys())
+    # games_data = list(recommended_games_json.values())
+
+    # for game_id, data in zip(game_ids[:25], games_data[:25]):
+    #     game_data = requests.get(f'https://store.steampowered.com/api/appdetails?appids={game_id}&lang=en').json()[game_id]['data']
+
+    #     score = data['positive'] / (data['positive'] + data['negative']) * 10
+    #     game = {
+    #         'name': game_data['name'],
+    #         'img': game_data['header_image'],
+    #         'rating': score.__round__(2),
+    #         'website': f'https://store.steampowered.com/agecheck/app/{game_id}/'
+    #     }
+    #     recommended_games.append(game)
+
+    recommended_games = list(database.top_games.find({}))
+    top_games = recommended_games
+
     if not steamID:
-        return render_template('index.html')
+        return render_template(
+            'index.html',
+            recommended_games=recommended_games,
+            top_games=top_games,
+        )
 
     user = steam.users.get_user_details(steamID)['player']
-    # owned_games = steam.users.get_owned_games(steamID)['games']
-    # steam_level = steam.users.get_user_steam_level(steamID)
-    # badges = steam.users.get_user_badges(steamID)
-
     return render_template(
         'index_logged_in.html',
         username=user['personaname'],
         avatar=user['avatarfull'],
+        recommended_games=recommended_games,
+        top_games=top_games
     )
 
 @app.route('/login')
@@ -69,6 +98,36 @@ def get_owned_games():
     else:
         return 'Please <a href="/?login=true">log in</a>'
 
+@app.route('/my_account')
+def my_account():
+    steamID = request.cookies.get('steam_id')
+
+    if steamID is not None:
+        user = steam.users.get_user_details(steamID)['player']
+        owned_games = steam.users.get_owned_games(steamID)['games']
+        steam_level = steam.users.get_user_steam_level(steamID)
+
+        games = []
+        for owned_game in owned_games:
+            app_id = owned_game['appid']
+
+            game_data = requests.get(f'https://store.steampowered.com/api/appdetails?appids={app_id}&lang=en').json()[str(app_id)]['data']
+            game = {
+                'name': game_data['name'],
+                'img': game_data['header_image'],
+                'website': f'https://store.steampowered.com/agecheck/app/{app_id}/'
+            }
+            games.append(game)
+
+        return render_template(
+            'account.html',
+            username=user['personaname'],
+            avatar=user['avatarfull'],
+            steam_level=steam_level['player_level'],
+            games=games,
+        )
+
 if __name__ == '__main__':
     app.run(port=80, host="127.0.0.1", debug=True) 
+
 
